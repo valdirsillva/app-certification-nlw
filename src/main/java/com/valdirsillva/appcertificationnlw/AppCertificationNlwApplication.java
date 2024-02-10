c5214483ef7ddc40e08aaa8f92b1c830cbb4d78a
Valdir Silva <valdipiresba@gmail.com>
2024-02-10 20:13:10 -0300

    setup do projeto

---
+++ src/main/java/com/valdirsillva/appcertificationnlw/AppCertificationNlwApplication.java
@@ -0,0 +1,13 @@
+package com.valdirsillva.appcertificationnlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCertificationNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCertificationNlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valdirsillva/appcertificationnlw/AppCertificationNlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdirsillva.appcertificationnlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCertificationNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
